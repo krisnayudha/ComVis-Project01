4d11d0f0965027e0e3d5a5dfb54542e81869f8a1
Krisna Yudha <krisnayudha17@gmail.com>
2021-11-09 22:02:29 +0700

    menambahkan file main.py dan cam-obj.py serta file gambar untuk testing shape

---
+++ Cam_Obj.py
@@ -0,0 +1,32 @@
+import cv2
+
+
+# image = cv2.imread("shapes1.png",1) # load image in color
+cap = cv2.VideoCapture(1)
+
+while True:
+    ret, image = cap.read()
+    cv2.imshow('Input', image)
+    if cv2.waitKey(1)==99:      # press "c" to capture
+        break
+
+# convert to grayscale
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+#find threshold of the image
+_, thrash = cv2.threshold(gray_image, 50, 255, cv2.THRESH_BINARY)
+contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+for contour in contours:
+	shape = cv2.approxPolyDP(contour, 0.01*cv2.arcLength(contour, True), True)
+	x_cor = shape.ravel()[0]
+	y_cor = shape.ravel()[1]
+
+	if len(shape) == 3:
+		cv2.drawContours(image, [shape], 0, (0,0,255), 4)
+		cv2.putText(image, "Triangle", (x_cor, y_cor), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,0,0))
+
+cv2.imshow("shape", image)
+cv2.imshow("Bin", thrash)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
