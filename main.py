87137fcb05f546ddde22e113e70fe4417a2b2a02
Krisna Yudha <krisnayudha17@gmail.com>
2021-11-20 17:48:58 +0700

    mengubah file main.py dan setting range hsv untuk warna kuning

--- main.py
+++ main.py
@@ -3,23 +3,47 @@
 import cv2 
 import numpy as np
 
-cap = cv2.VideoCapture(1)
+def nothing(x):
+	#any operation
+	pass
+
+cap = cv2.VideoCapture(0)
+
+cv2.namedWindow('Trackbars')
+cv2.createTrackbar('l_h', 'Trackbars', 0, 255, nothing)
+cv2.createTrackbar('l_s', 'Trackbars', 0, 255, nothing)
+cv2.createTrackbar('l_v', 'Trackbars', 0, 255, nothing)
+cv2.createTrackbar('u_h', 'Trackbars', 0, 255, nothing)
+cv2.createTrackbar('u_s', 'Trackbars', 0, 255, nothing)
+cv2.createTrackbar('u_v', 'Trackbars', 0, 255, nothing)
 
 while True:
 	ret, frame = cap.read()
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+	# get values from trackars 
+	l_h = cv2.getTrackbarPos('l_h', 'Trackbars')
+	l_s = cv2.getTrackbarPos('l_s', 'Trackbars')
+	l_v = cv2.getTrackbarPos('l_s', 'Trackbars')
+	u_h = cv2.getTrackbarPos('u_h', 'Trackbars')
+	u_s = cv2.getTrackbarPos('u_s', 'Trackbars')
+	u_v = cv2.getTrackbarPos('u_s', 'Trackbars')
+
+
+	lower_yellow = np.array([l_h, l_s, l_v]) # 15, 93, 243
+	upper_yellow = np.array([u_h, u_s, u_v]) # 53, 219, 211
+
+	mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+
+	yellow = cv2.bitwise_and(hsv, hsv, mask=mask)
+
 	cv2.imshow('input', frame)
-	if cv2.waitkey(1) == 99:		# press "C" to capture
+	cv2.imshow('mask', mask)
+	cv2.imshow('yellow', yellow)
+
+
+	if cv2.waitKey(1) == 27:		# press "esc" to exit
 		break
 
 cap.release()
-
-cv2.imshow('Captured Image', frame)
-hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-lower_yellow = np.array9([0, 199, 204])
-upper_yellow = np.array([155, 253, 255])
-mask1 = cv2.inRange(hsv, lower_yellow, upper_yellow)
-
-bit_yellow = cv2.bitwise_and(hsv, hsv, mask=mask1)
-imgyellow = cv2.cvtColor(bit_yellow, cv2.COLOR_HSV2BGR)
-
-cv2.imshow('yellow', imgyellow)
+cv2.destroyAllWindows()
