d9bd70f9fb87b6a408a186ad15fcdfa2b340b28e
Krisna Yudha <krisnayudha17@gmail.com>
2021-11-26 11:41:04 +0700

    main.py untuk mencari nilai hsv, cam_obj.py program deteksi object

--- Cam_Obj.py
+++ Cam_Obj.py
@@ -2,69 +2,122 @@ import cv2
 import numpy as np
 import PySimpleGUI as sg
 
-head = [['Jumlah Segitiga', 'Posisi 1', 'Posisi 2', 'Posisi 3']]
-data = [['Small', 0, 0, 0],
-		['Medium',0, 0, 0],
-		['Large', 0, 0, 0]]
-layout = [[sg.Image('blank640x480.png', key = 'input'),sg.Image('balnk640x480.png', key = 'impro')],
+head = ['Jumlah Segitiga','Posisi 1','Posisi 2','Posisi 3']
+data = [['Small', 0,0,0,0],
+		['Medium',0,0,0,0],
+		['Large',0,0,0,0]]
+layout = [[sg.Image('blank640x480.png', key = 'input'),sg.Image('blank640x480.png', key = 'impro')],
 			[sg.Button('Captured'), 
-			sg.Button('Exit'),
-			sg.Table(data, headings=head,hide_vertical_scroll=True, justification='center', key='table', num_rows=4)],
+			sg.Button('Exit'), 
+			sg.Table(data,headings = head,hide_vertical_scroll=True, justification='center', key='table', num_rows=3,auto_size_columns=True)
+			],
 			[]]
 
 window = sg.Window('Triangle detection and Position', layout)
 
+
 # image = cv2.imread("shapes1.png",1) # load image in color
 cap = cv2.VideoCapture(1)
 
+
+
 while True : 
-	event, values = window.read(timeout=10)
+	event, values = window.read(0.01)
 	ret, Frame = cap.read()
 	imgbytes = cv2.imencode('.png', Frame)[1].tobytes()
 	window['input'].update(data=imgbytes)
 
-	# find object with yellow color
-	hsv = cv2.cvtColor(Frame, cv2.COLOR_BGR2HSV)
-	lower_yellow = np.array([0, 199, 204])
-	upper_yellow = np.array([155, 253, 255])
-	mask1 = cv2.inRange(hsv, lower_yellow, upper_yellow)
+	if event == 'Captured':
+		smallposisi1 = 0
+		smallposisi2 = 0
+		smallposisi3 = 0
+		mediumposisi1 = 0
+		mediumposisi2 = 0
+		mediumposisi3 = 0
+		largeposisi1 = 0
+		largeposisi2 = 0
+		largeposisi3 = 0
+		# capture object
+		objectFrame = Frame
 
-	# convert to grayscale
-	gray_image = cv2.cvtColor(Frame, cv2.COLOR_BGR2GRAY)
+		# find object with yellow color
+		hsv = cv2.cvtColor(objectFrame, cv2.COLOR_BGR2HSV)
+		lower_yellow = np.array([15,93,0])
+		upper_yellow = np.array([40,255,255])
 
-	# find threshold of the image
-	_, thrash = cv2.threshold(gray_image, 50, 255, cv2.THRESH_BINARY)
-	contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+		# set kernel to filtering mask
+		kernel = np.ones((15,15),np.uint8)
 
-	# find the centre of the object
-	cnt = contours[0]
-	M = cv2.moments(cnt)
+		# threshold the hsv image to get only yellow color
+		mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+		img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+		img = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+		imghsv = cv2.bitwise_and(hsv, hsv, mask=mask)
 
+		# find triangle object with approxPoly
+		contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-	for contour in contours:
-		area = cv2.contourArea(contours[contour])
-		x,y,w,h = cv2.boundingRect(contours[contour])
-		rect_area = w*h
-		shape = cv2.approxPolyDP(contour, 0.01*cv2.arcLength(contour, True), True)
-		cx = int(M['m10']/M['m00'])
-		cy = int(M['m01']/M['m00'])
+		# imgbytes = cv2.imencode('.png', mask)[1].tobytes()
+		# window['impro'].update(data=imgbytes)
 
-		# x_cor = shape.ravel()[0]
-		# y_cor = shape.ravel()[1]
-
-		# if len(shape) == 3:
-		# 	cv2.drawContours(Frame, [shape], 0, (0,0,255), 4)
-		# 	cv2.putText(Frame, "Triangle", (x_cor, y_cor), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,0,0))
-		if len(shape) == 3:
-			if rect_area <= 2000:
-				ukuran = 'S'
-			elif 2001 <= rect_area <= 5000:
-				ukuran = 'M'
-			elif 5001 <= rect_area <= 8000:
-				ukuran = 'L'
+		for contour in contours:
+			area = cv2.contourArea(contour)
+			shape = cv2.approxPolyDP(contour, 0.1*cv2.arcLength(contour, True), True)
+			x,y,w,h = cv2.boundingRect(shape)
 			
+			if len(shape) == 3:
+				rect = cv2.minAreaRect(contour)
+				box = cv2.boxPoints(rect)
+				border = np.int0(box)
+				borderImg = cv2.drawContours(objectFrame,[border], 0, (255, 0 , 255), 2)
+				# #  calculate moments for each cotour
+				M = cv2.moments(contour)
 
-# cv2.imshow("shape", Frame)
-# cv2.imshow("Bin", thrash)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+				# calculate x,y coordinate of center 
+				if M['m00'] != 0:
+					cX = int(M['m10']/M['m00'])
+					cY = int(M['m01']/M['m00'])
+				else:
+					cX, cY = 0,0
+
+				cv2.putText(borderImg, "points: " + str(len(shape)), (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 1)
+				cv2.putText(borderImg, "area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 1)
+				cv2.circle(borderImg, (cX, cY), 2, (0,255,0), -1)
+				cv2.putText(borderImg, "center: " + str(int(cX)) + "," + str(int(cY)), (x + w + 20, y + 70), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 1)
+				imgbytes = cv2.imencode('.png', borderImg)[1].tobytes()
+				window['impro'].update(data=imgbytes)
+
+				if area <= 1000:
+					ukuran = 'S'
+					if cX <= 210:
+						smallposisi1 = smallposisi1 + 1
+					if 210 < cX <= 420:
+						smallposisi2 = smallposisi2 + 1
+					if cX > 420:
+						smallposisi3 = smallposisi3 + 1
+				elif 1000 < area <= 3000:
+					ukuran = 'M'
+					if cX <= 210:
+						mediumposisi1 = mediumposisi1 + 1
+					if 210 < cX <= 420:
+						mediumposisi2 = mediumposisi2 + 1
+					if cX > 420:
+						mediumposisi3 = mediumposisi3 + 1
+				elif area > 3000:
+					if cX <= 210:
+						largeposisi1 = largeposisi1 + 1
+					if 210 < cX <= 420:
+						largeposisi2 = largeposisi2 + 1
+					if cX > 420:
+						largeposisi3 = largeposisi3 + 1
+
+		data = [['Small', smallposisi1,smallposisi2,smallposisi3],
+				['Medium', mediumposisi1,mediumposisi2,mediumposisi3],
+				['Large', largeposisi1,largeposisi2,largeposisi3]]
+
+		window['table'].update(data)
+	elif event == 'Exit':
+		break
+
+cap.release()
+window.close()
--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def nothing(x):
 	#any operation
 	pass
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
+# img = cv2.imread('shapes1.png', 1)
 
 cv2.namedWindow('Trackbars')
 cv2.createTrackbar('l_h', 'Trackbars', 0, 255, nothing)
@@ -21,29 +23,40 @@ while True:
 	ret, frame = cap.read()
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	# get values from trackars 
+	# get values from trackbars 
 	l_h = cv2.getTrackbarPos('l_h', 'Trackbars')
 	l_s = cv2.getTrackbarPos('l_s', 'Trackbars')
-	l_v = cv2.getTrackbarPos('l_s', 'Trackbars')
+	l_v = cv2.getTrackbarPos('l_v', 'Trackbars')
 	u_h = cv2.getTrackbarPos('u_h', 'Trackbars')
 	u_s = cv2.getTrackbarPos('u_s', 'Trackbars')
-	u_v = cv2.getTrackbarPos('u_s', 'Trackbars')
+	u_v = cv2.getTrackbarPos('u_v', 'Trackbars')
 
 
-	lower_yellow = np.array([l_h, l_s, l_v]) # 15, 93, 243
-	upper_yellow = np.array([u_h, u_s, u_v]) # 53, 219, 211
+	lower_yellow = np.array([l_h, l_s, l_v]) # 15, 93, 0
+	upper_yellow = np.array([u_h, u_s, u_v]) # 40, 255, 255
+	
+	# lower_yellow = np.array([15,93,0])
+	# upper_yellow = np.array([40,255,255])
 
+	# set kernel to filtering mask
+	kernel = np.ones((15,15),np.uint8)
+
+	# threshold the hsv image to get only yellow color
 	mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-
-	yellow = cv2.bitwise_and(hsv, hsv, mask=mask)
+	img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+	img = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+	
+	imghsv = cv2.bitwise_and(hsv, hsv, mask=mask)
 
 	cv2.imshow('input', frame)
 	cv2.imshow('mask', mask)
-	cv2.imshow('yellow', yellow)
+	cv2.imshow('yellow', imghsv)
+
 
 
 	if cv2.waitKey(1) == 27:		# press "esc" to exit
 		break
 
-cap.release()
+# cv2.waitKey(0)
+# cap.release()
 cv2.destroyAllWindows()
